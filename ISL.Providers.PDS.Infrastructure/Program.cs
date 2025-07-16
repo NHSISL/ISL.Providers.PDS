99e518438c3b02897bca3ab859ee1f744d089b49
Christo du Toit <christo@dutoit.co.uk>
2025-07-16 11:16:31 +0100

    INFRA: Initial project setup

---
+++ ISL.Providers.PDS.Infrastructure/Program.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using ISL.Providers.PDS.Infrastructure.Services;
+
+namespace ISL.Providers.PDS.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+
+            scriptGenerationService.GenerateBuildScript(
+                branchName: "main",
+                projectName: "ISL.Providers.PDS.Abstractions",
+                dotNetVersion: "9.0.100");
+
+            scriptGenerationService.GeneratePrLintScript("main");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# ISL.Providers.PDS
+
+This repository contains the SPAL source code for the 
+ISL Re-Identification Abstraction library as well as the 
+FHIR and an OfflineFileSource provider implementation.
+
