a019fbe8d71eb735fae857c8710a41956a77b9aa
Joe Owens <joe.owens@answerdigital.com>
2025-07-24 12:00:47 +0100

    FOUNDATIONS: Logic tests for PdsService

---
+++ ISL.Providers.PDS.FHIR.Tests.Unit/Services/Foundations/Pds/PdsServiceTests.Logic.PatientLookupByDetailsAsync.cs
@@ -0,0 +1,71 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using Moq;
+using Force.DeepCloner;
+using ISL.Providers.PDS.Abstractions.Models;
+using Hl7.Fhir.Model;
+using FluentAssertions;
+using Task = System.Threading.Tasks.Task;
+
+namespace ISL.Providers.PDS.FHIR.Tests.Unit.Services.Foundations.Pds
+{
+    public partial class PdsServiceTests
+    {
+        [Fact]
+        public async Task ShouldPatientLookupByDetailsAsync()
+        {
+            // Given
+            string randomString = GetRandomString();
+            string inputSurname = randomString.DeepClone();
+
+            Bundle outputBundle = CreateRandomBundle(inputSurname);
+            PatientBundle expectedPatientBundle = CreateRandomPatientBundle(outputBundle);
+
+            this.pdsFHIRBrokerMock.Setup(broker =>
+                broker.GetNhsNumberAsync(
+                    null,
+                    inputSurname,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null))
+                .ReturnsAsync(outputBundle);
+
+            // When
+            PatientBundle actualPatientBundle = await pdsService
+                .PatientLookupByDetailsAsync(
+                    null,
+                    inputSurname,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null);
+
+            // Then
+            actualPatientBundle.Should().BeEquivalentTo(expectedPatientBundle);
+
+            this.pdsFHIRBrokerMock.Verify(broker =>
+                broker.GetNhsNumberAsync(
+                    null,
+                    inputSurname,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null),
+                        Times.Once);
+
+            this.pdsFHIRBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
